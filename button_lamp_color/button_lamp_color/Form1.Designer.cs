f67433bf0125107fdaf2fb97595dfd75dc9046a6
kyu-jung <bongseo1012@gmail.com>
2025-04-21 13:18:23 +0900

    Add files via upload

---
+++ button_lamp_color/button_lamp_color/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace button_lamp_color
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(138, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "색변환 Button";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(94, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(644, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "연속_횟수 지정 버튼을 이용한 램프 색 변환";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(35, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 82);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "연속 점멸";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(288, 411);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 82);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "연속(횟수지정)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button4.Location = new System.Drawing.Point(546, 411);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 82);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "초기화";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(503, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 38);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "색변환 Label";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(69, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(284, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "연속(획수지정) 횟수 설정";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(558, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "동작횟수 :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(687, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "0";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.numericUpDown1.Location = new System.Drawing.Point(367, 315);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(134, 39);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 558);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lamp 연속제어하기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ button_lamp_color/button_lamp_color/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button_lamp_color
+{
+    public partial class Form1: Form
+    {
+        int A = 0;
+        int Auto = 0;
+        int Count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //연속 점멸 (타이머 구동)
+        {
+            timer1.Start();
+            timer1.Interval = 300;
+            A = 1;
+            Auto = 0;
+            Count = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e) // 연속 횟수지정 (타이머 구동)
+        {
+            timer1.Start();
+            timer1.Interval = 300;
+            A = 2;
+            Auto = 0;
+            Count = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e) // 초기화
+        {
+            timer1.Stop();
+            A = 0;
+            Auto = 0;
+            Count = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // 타이머 동작프로그램
+        {
+            label5.Text = Count.ToString(); // 현재 카운트 표시
+
+            int Counting = Convert.ToInt32(numericUpDown1.Value); 
+
+            if ((A == 1) || (A == 2 && Count < Counting)) 
+            {
+                switch (Auto)
+                {
+                    case 0:
+                        if (button1.BackColor == Color.White)
+                        {
+                            button1.BackColor = Color.Aqua;
+                            button1.ForeColor = Color.White;
+                            button1.Text = "점등";
+                            label2.BackColor = Color.DarkOrange;
+                            label2.ForeColor = Color.Indigo;
+                            label2.Text = "점등";
+
+                            Auto++;
+                        }
+                        break;
+                    case 1:
+                        if (button1.BackColor == Color.Aqua)
+                        {
+                            button1.BackColor = Color.White;
+                            button1.ForeColor = Color.Blue;
+                            button1.Text = "소등";
+                            label2.BackColor = Color.OrangeRed;
+                            label2.ForeColor = Color.White;
+                            label2.Text = "소등";
+
+                            Auto++;
+                            Count++;
+                        }
+                        break;
+                    case 2:
+                        Auto = 0;
+                        break;
+
+                }            
+            }
+        }
+    }
+}
